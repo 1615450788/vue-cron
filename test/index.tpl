f3a6f4a2f27b009f4fe9d0245e24ff9a6784c346
wangtao <1615450788@qq.com>
2017-12-08 16:45:25 +0800

    demo优化

--- test/index.tpl
+++ test/index.tpl
@@ -4,6 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title><%= htmlWebpackPlugin.options.title %></title>
+    <style>
+      body{
+        background: #eee;
+      }
+    </style>
   </head>
   <body>
     <div id="app">
--- test/test.vue
+++ test/test.vue
@@ -1,10 +1,19 @@
 <style lang="less" scoped>
-
+    .cron{
+        width: 400px;
+        margin: 0 auto;
+        margin-top: 100px;
+        h1{
+            font-size: 50px;
+            text-align: center;
+        }
+    }
 </style>
 
 <template>
-    <div>
-        <el-popover v-model="cronPopover" placement="right">
+    <div class="cron">
+        <h1>vue-cron</h1>
+        <el-popover v-model="cronPopover">
             <cron :data="cron" @change="changeCron" @close="cronPopover=false"></cron>
             <el-input slot="reference" @click="cronPopover=true" v-model="cron" placeholder="请输入定时策略"></el-input>
         </el-popover>
